98e4e90c29119b05c44d728d7fda996735c78a86
Akito Dataminer <akito.dataminer@gmail.com>
2022-04-26 10:41:22 +0900

    add the snippet for configure ipv4.

--- LuaSnip-snippets/c.lua
+++ LuaSnip-snippets/c.lua
@@ -19,12 +19,27 @@ local fmta = require("luasnip.extras.fmt").fmta
 local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.expand_conditions")
 
+-- args is a table, where 1 is the text in Placeholder 1, 2 the text in
+-- placeholder 2,...
+local function copy(args)
+  return args[1]
+end
+
 ls.add_snippets("c", {
-  s({trig = "block_comment"}, {
+  s({trig = "comment_block_short"}, {
+    t({"/* "}), i(1), t({" */"}),
+  }),
+  s({trig = "comment_block"}, {
     t({"/****************************************"}),
     t({"", " * "}), i(1),
     t({"", " ****************************************/"}),
-  })
+  }),
+  s({trig = "ipv4_config"}, {
+    i(1, {"sockaddr_in"}), t({".sin_family = AF_INET;"}),
+    t({"", ""}), f(copy, 1), t({".sin_port = htons("}), i(2), t({");"}),
+    t({"", ""}), f(copy, 1), t({".sin_addr.S_un.S_addr = "}), i(3), t({";"}),
+    i(0),
+  }),
 }, {
     key = "c",
   })

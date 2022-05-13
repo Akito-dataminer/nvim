a83f565b0a31fb7917d1fdd8bd31e4b8d2648930
Akito Dataminer <akito.dataminer@gmail.com>
2022-05-13 14:48:51 +0900

    add the cpp-snippet to specify parent class(es)

--- LuaSnip-snippets/cpp.lua
+++ LuaSnip-snippets/cpp.lua
@@ -8,16 +8,6 @@ local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
-local l = require("luasnip.extras").lambda
-local rep = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.expand_conditions")
 
 -- args is a table, where 1 is the text in Placeholder 1, 2 the text in
 -- placeholder 2,...
@@ -27,7 +17,11 @@ end
 
 ls.add_snippets("cpp", {
   s("class", {
-    t({"class "}), i(1), t({" {"}),
+    t({"class "}), i(1),
+    c(2, {
+      t(""),
+      sn( nil, { t(" : "), i(1) } ),
+    } ), t({" {"}),
     t({"", "private:"}),
     t({"", "public:"}),
     t({"", "\t"}), f(copy, 1), t({"();"}),
--- lua/PluginConfig/LuaSnip.lua
+++ lua/PluginConfig/LuaSnip.lua
@@ -6,32 +6,32 @@ local types = require("luasnip.util.types")
 
 -- Every unspecified option will be set to the default.
 ls.config.set_config({
-	history = true,
-	-- Update more often, :h events for more info.
-	updateevents = "TextChanged,TextChangedI",
-	-- Snippets aren't automatically removed if their text is deleted.
-	-- `delete_check_events` determines on which events (:h events) a check for
-	-- deleted snippets is performed.
-	-- This can be especially useful when `history` is enabled.
-	delete_check_events = "TextChanged",
-	ext_opts = { [types.choiceNode] = { active = { virt_text = { { "choiceNode", "Comment" } } } } },
-	-- treesitter-hl has 100, use something higher (default is 200).
-	ext_base_prio = 300,
-	-- minimal increase in priority.
-	ext_prio_increase = 1,
-	enable_autosnippets = true,
-	-- mapping for cutting selected text so it's usable as SELECT_DEDENT,
-	-- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
-	store_selection_keys = "<Tab>",
-	-- luasnip uses this function to get the currently active filetype. This
-	-- is the (rather uninteresting) default, but it's possible to use
-	-- eg. treesitter for getting the current filetype by setting ft_func to
-	-- require("luasnip.extras.filetype_functions").from_cursor (requires
-	-- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
-	-- the current filetype in eg. a markdown-code block or `vim.cmd()`.
-	ft_func = function()
-		return vim.split(vim.bo.filetype, ".", true)
-	end,
+  history = true,
+  -- Update more often, :h events for more info.
+  updateevents = "TextChanged,TextChangedI",
+  -- Snippets aren't automatically removed if their text is deleted.
+  -- `delete_check_events` determines on which events (:h events) a check for
+  -- deleted snippets is performed.
+  -- This can be especially useful when `history` is enabled.
+  delete_check_events = "TextChanged",
+  ext_opts = { [types.choiceNode] = { active = { virt_text = { { "choiceNode", "Comment" } } } } },
+  -- treesitter-hl has 100, use something higher (default is 200).
+  ext_base_prio = 300,
+  -- minimal increase in priority.
+  ext_prio_increase = 1,
+  enable_autosnippets = true,
+  -- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+  -- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+  -- store_selection_keys = "<Tab>",
+  -- luasnip uses this function to get the currently active filetype. This
+  -- is the (rather uninteresting) default, but it's possible to use
+  -- eg. treesitter for getting the current filetype by setting ft_func to
+  -- require("luasnip.extras.filetype_functions").from_cursor (requires
+  -- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
+  -- the current filetype in eg. a markdown-code block or `vim.cmd()`.
+  ft_func = function()
+    return vim.split(vim.bo.filetype, ".", true)
+  end,
 })
 
 -- in a cpp file: search cpp-snippets, c-snippets, then all-snippets.
@@ -50,7 +50,7 @@ require("luasnip.loaders.from_lua").lazy_load({ paths = "./non-program-snippets"
 -- One peculiarity of honza/vim-snippets is that the file with the global snippets is _.snippets, so global snippets
 -- are stored in `ls.snippets._`.
 -- We need to tell luasnip that "_" contains global snippets:
-ls.filetype_extend("all", { "_" })
+-- ls.filetype_extend("all", { "_" })
 
 -- require("luasnip.loaders.from_snipmate").load({include = {"c"}}) -- Load only python snippets
 
@@ -58,11 +58,9 @@ ls.filetype_extend("all", { "_" })
 -- If path is not specified, luasnip will look for the `snippets` directory in rtp (for custom-snippet probably
 -- `~/.config/nvim/snippets`).
 
--- require("luasnip.loaders.from_snipmate").lazy_load() -- Lazy loading
-
 -- vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
 -- vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
 -- vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
 -- vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
--- vim.api.nvim_set_keymap("i", "<C-Down>", "<Plug>luasnip-next-choice", {})
--- vim.api.nvim_set_keymap("s", "<C-Down>", "<Plug>luasnip-next-choice", {})
+-- vim.api.nvim_set_keymap("i", "<Tab>", "<Plug>luasnip-next-choice", {})
+-- vim.api.nvim_set_keymap("s", "<Tab>", "<Plug>luasnip-next-choice", {})

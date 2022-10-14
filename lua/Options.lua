cc139843b4f75c918553c31280caf753dda06134
Akito-dataminer <akito.dataminer@gmail.com>
2022-10-14 17:12:26 +0900

    add - a setting for disable mouse

--- lua/Options.lua
+++ lua/Options.lua
@@ -36,6 +36,7 @@ opt.title = true
 opt.termguicolors = true
 opt.pumblend = 17
 opt.updatetime = 200 -- time to highlight
+opt.mouse = "" -- disable mouse
 vg.loaded_netrw = false -- disable netrw
 vg.loaded_netrwPlugin = false -- disable netrw plugins
 

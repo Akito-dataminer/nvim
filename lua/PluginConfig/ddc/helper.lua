7355148ff1c661293eaf780b04c156f7d9912e83
Akito Dataminer <akito.dataminer@gmail.com>
2023-09-10 13:17:11 +0900

    add - helper functions to config ddc

---
+++ lua/PluginConfig/ddc/helper.lua
@@ -0,0 +1,17 @@
+local fn = vim.fn
+
+local M = {}
+
+function M.patch_global(...)
+  fn["ddc#custom#patch_global"](...)
+end
+
+function M.patch_filetype(...)
+  fn["ddc#custom#patch_filetype"](...)
+end
+
+function M.enable(...)
+  fn["ddc#enable"](...)
+end
+
+return M;

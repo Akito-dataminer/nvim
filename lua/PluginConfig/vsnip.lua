57f569ede47183292597f90a6a025b73edad2559
Akito Dataminer <akito.dataminer@gmail.com>
2023-09-12 11:35:06 +0900

    modify - to use vim.keymap.set in vsnip settings

--- lua/PluginConfig/vsnip.lua
+++ lua/PluginConfig/vsnip.lua
@@ -11,39 +11,18 @@ local data_path = fn.stdpath( "data" )
 local vsnip_dir = utils.join_paths( data_path, ".vsnip" )
 vg.vsnip_snippet_dir = vsnip_dir
 
-vg.vsnip_filetypes = {
-  cppreact = 'cpp',
-  javascriptreact = 'javascript',
-  typescriptreact = 'typescript',
-}
-
 -- Key Mappings
--- imap <expr> <C-j> vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'
--- smap <expr> <C-j> vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'
-cmd([[
-imap <expr> <Tab> vsnip#jumpable(1)    ? '<Plug>(vsnip-jump-next)' : '<Tab>'
-smap <expr> <Tab> vsnip#jumpable(1)    ? '<Plug>(vsnip-jump-next)' : '<Tab>'
-imap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
-smap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
-]])
-
--- local function map( mode, lhs, rhs, opts )
---   local options = {noremap = true}
---   if opts then options = vim.tbl_extend('force', options, opts) end
---   api.nvim_set_keymap( mode, lhs, rhs, options )
--- end
+api.nvim_create_autocmd('InsertEnter', {
+  callback = function(ev)
+    vim.keymap.set({ 'i', 's' }, '<Tab>',
+      function() return vim.fn['vsnip#jumpable'](1) == 1 and '<Plug>(vsnip-jump-next)' or '<Tab>' end,
+      { expr = true, noremap = false })
+    vim.keymap.set({ 'i', 's' }, '<S-Tab>',
+      function() return vim.fn['vsnip#jumpable'](-1) == 1 and '<Plug>(vsnip-jump-prev)' or '<S-Tab>' end,
+      { expr = true, noremap = false })
+  end,
+})
 
 -- local function termcode(str)
 --   return api.nvim_replace_termcodes(str, true, true, true)
 -- end
-
--- function _G.smartNext( key )
---   return fn.pumvisible() == 1 and termcode'<C-n>' or termcode( key )
--- end
-
--- function _G.expandable( key )
---   return fn['vsnip#expandable()'] == 1 and termcode'<Plug>(vsnip-expand)' or termcode( key )
--- end
-
--- local opts = { expr = true, noremap = true }
--- map( 'i', 'C-j', expandable('C-j'), opts )

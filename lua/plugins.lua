e9d7083c6b50980ae0d5cdad4a540e0b2e3b96ea
Akito Dataminer <akito.dataminer@gmail.com>
2023-10-02 15:20:00 +0900

    remove - redundant path delimiter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -210,5 +210,5 @@ require("lazy").setup(plugin_list, {
   defaults = {
     lazy = true, -- should plugins be lazy-loaded?
   },
-  lockfile = util.join_paths(vim.fn.stdpath("config"), "/lazy-lock.json"),
+  lockfile = util.join_paths(vim.fn.stdpath("config"), "lazy-lock.json"),
 })

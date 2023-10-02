9b853b6c2ad614fb25e11a303f85fffa2417d8d9
Akito Dataminer <akito.dataminer@gmail.com>
2023-10-02 15:25:06 +0900

    modify - the keymaps

--- lua/PluginConfig/mini-surround.lua
+++ lua/PluginConfig/mini-surround.lua
@@ -4,10 +4,10 @@ require('mini.surround').setup({
   mappings = {
     add = 'ca',            -- Add surrounding in Normal and Visual modes
     delete = 'cd',         -- Delete surrounding
-    find = 'cf',           -- Find surrounding (to the right)
-    find_left = 'cF',      -- Find surrounding (to the left)
+    find = 'c<',           -- Find surrounding (to the right)
+    find_left = 'c>',      -- Find surrounding (to the left)
     highlight = 'ch',      -- Highlight surrounding
-    replace = 'cr',        -- Replace surrounding
+    replace = 'cs',        -- Replace surrounding
     update_n_linec = 'cn', -- Update `n_lines`
     update_n_lines = '',
 

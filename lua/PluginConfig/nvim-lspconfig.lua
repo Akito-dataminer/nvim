902621719cb3d4a1fb5df886eab68dc195319f9d
Akito <102844371+Akito-dataminer@users.noreply.github.com>
2023-10-08 15:19:21 +0900

    bugfix - I made a mistake with the settings for the formatter embedded in lsp (#14)

--- lua/PluginConfig/nvim-lspconfig.lua
+++ lua/PluginConfig/nvim-lspconfig.lua
@@ -57,9 +57,8 @@ local my_on_attach = function(client, bufnr)
   keymap.set('n', 'gr', lsp.buf.references, bufopts)
 
   if client.server_capabilities.documentFormattingProvider then
-    keymap.set( 'n', '<space>f', vim.lsp.buf.format(), {noremap = false, silent = true, buffer = bufnr} )
+    keymap.set('n', '<space>f', function() lsp.buf.format { async = true } end, { noremap = false, silent = true, buffer = bufnr })
   end
-  -- keymap.set('n', '<space>f', function() lsp.buf.format { async = true } end, bufopts)
 
   -- Find the clients capabilities
   local doc_light = client.server_capabilities.documentHighlightProvider

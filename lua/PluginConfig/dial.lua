de4f258619c9bcf283735d5f4192c3a4db236f93
Akito-dataminer <akito.dataminer@gmail.com>
2025-03-19 23:40:55 +0900

    feat: add configuration for dial.nvim

---
+++ lua/PluginConfig/dial.lua
@@ -0,0 +1,37 @@
+local augend = require("dial.augend")
+require("dial.config").augends:register_group({
+  default = {
+    augend.integer.alias.decimal,
+    augend.integer.alias.hex,
+    augend.integer.alias.binary,
+    augend.date.alias["%Y/%m/%d"],
+    augend.constant.alias.bool,
+    augend.constant.alias.alpha,
+    augend.constant.alias.Alpha,
+  },
+})
+
+vim.keymap.set("n", "<C-a>", function()
+  require("dial.map").manipulate("increment", "normal")
+end)
+vim.keymap.set("n", "<C-x>", function()
+  require("dial.map").manipulate("decrement", "normal")
+end)
+vim.keymap.set("n", "g<C-a>", function()
+  require("dial.map").manipulate("increment", "gnormal")
+end)
+vim.keymap.set("n", "g<C-x>", function()
+  require("dial.map").manipulate("decrement", "gnormal")
+end)
+vim.keymap.set("v", "<C-a>", function()
+  require("dial.map").manipulate("increment", "visual")
+end)
+vim.keymap.set("v", "<C-x>", function()
+  require("dial.map").manipulate("decrement", "visual")
+end)
+vim.keymap.set("v", "g<C-a>", function()
+  require("dial.map").manipulate("increment", "gvisual")
+end)
+vim.keymap.set("v", "g<C-x>", function()
+  require("dial.map").manipulate("decrement", "gvisual")
+end)
